7b5e4bb29b1073165a3612bee249551f1d022d2c
oussama <oussamachabbouh123@gmail.com>
2025-02-21 17:19:53 +0100

    projet next

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+    return (
+      <div>
+        <h1>Welcome to Home Page</h1>
+        <p>This is the /home page.</p>
+      </div>
+    );
+  }
+  
---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Accueil from "./acceuil/Accueil";
+import Header from "./acceuil/Header";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+        <Header />
+        <div>
+      <h1>Welcome to the Main Page</h1>
+      <Link href="/home">Go to Home Page</Link>
+    </div>
+        <Accueil />
+        </>
+  );
+}
